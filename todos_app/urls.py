27a27f1dbd9ca8e371ab8987bcd4020bb0c0c7ba
malte <malte@flex.earth>
2022-05-23 01:17:08 +0200

    Views for adding/editing/deleting

--- todos_app/urls.py
+++ todos_app/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('imprint', views.imprint)
+    path('imprint', views.imprint),
+    path('add', views.add),
+    path('create', views.create),
+    path('edit/<int:todo_id>', views.edit),
+    path('save/<int:todo_id>', views.save),
+    path('delete/<int:todo_id>', views.delete),
 ]
--- todos_app/views.py
+++ todos_app/views.py
@@ -14,3 +14,31 @@ def index(request):
 
 def imprint(request):
     return render(request, 'todos_app/imprint.html')
+
+def add(request):
+    return render(request, 'todos_app/edit.html')
+
+def create(request):
+    todo = Todo(title=request.POST['title'], deadline=request.POST['deadline'], percent_done=request.POST['percent_done'])
+    todo.save()
+    return redirect("/")
+
+def edit(request, todo_id):
+    todo = get_object_or_404(Todo, id=todo_id)
+    context = {
+        'todo': todo,
+    }
+    return render(request, 'todos_app/edit.html', context)
+
+def save(request, todo_id):
+    todo = get_object_or_404(Todo, id=todo_id)
+    todo.title = request.POST['title']
+    todo.deadline = request.POST['deadline']
+    todo.percent_done = request.POST['percent_done']
+    todo.save()
+    return redirect("/")
+
+def delete(request, todo_id):
+    todo = get_object_or_404(Todo, id=todo_id)
+    todo.delete()
+    return redirect("/")
