9c1874f9792499551e9766a334679c50fb07ff54
malte <malte@flex.earth>
2022-05-23 00:27:14 +0200

    Add index view

--- todos_app/views.py
+++ todos_app/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Todo
+
 
 def index(request):
-    return HttpResponse("Hello!")
+    todos_list = Todo.objects.all()
+    context = {
+        'todos_list': todos_list,
+    }
+    return render(request, 'todos_app/index.html', context)
